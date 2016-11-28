5e2d0f84d7f073f82bf51ee84265259ead01f108
Don Dini <dd130v@att.com>
2016-11-28 11:42:55 -0800

    Created larger sample network for data sampling.

--- network.py
+++ network.py
@@ -61,6 +61,17 @@ class Node:
         for tuple_value in bindings:
             self.cpt[tuple_value] = [float(1)/len(self.values) for v in self.values]
     
+    def get_variable_combinations(self):
+        val_lists = [p.values for p in self.parents]
+        
+        combinations = None 
+        
+        elem_it = itertools.product(*val_lists)
+
+        combinations = [e for e in elem_it]
+
+        return combinations
+    
     def add_parent(self, aNode):
         self.parents.append(aNode)
     
@@ -169,7 +180,9 @@ class BeliefNetwork:
 
             Assumes 'self.nodes' are in topological sorted order.
         """
-        
+
+        logger.info("Sampling 1 record.")
+
         #Acquire nodes sorted in topological order
         sorted_nodes = self.nodes
 
@@ -178,16 +191,14 @@ class BeliefNetwork:
         for n in sorted_nodes:
 
             if bindings is not None and n.id in bindings:
-                logger.info("Using bound value: %s" % str(bindings[n.id]) )
+                #logger.info("Using bound value: %s" % str(bindings[n.id]) )
                 nodes_to_value[n.id] = bindings[n.id]
             else:
-                logger.info("Getting sample for %s" % n.id)
+                #logger.info("Getting sample for %s" % n.id)
 
                 #Determine value of parent nodes
                 #-------------------------------
                 parent_ids = [p.id for p in n.parents]
-
-                logger.info("Parents: %s" % ",".join(parent_ids))
 
                 parent_node_vals = {}
                 
@@ -198,7 +209,7 @@ class BeliefNetwork:
                 #Acquire value for this node
                 sampled_value = n.sample(parent_node_vals)
 
-                logger.info("sampled value for %s: %s" % (n.id, sampled_value))
+                #logger.info("sampled value for %s: %s" % (n.id, sampled_value))
 
                 nodes_to_value[n.id] = sampled_value
         
@@ -206,6 +217,199 @@ class BeliefNetwork:
     
     def evaluate(self, bindings):
         pass
+
+def notebook_data():
+
+    nodeY = Node([0,1], id="Y")
+
+    nodeX1 = Node([0,1], id="X1")
+
+    nodeX2 = Node([0,1], id="X2")
+    #Subtree of variables
+    node2_subnodes = []
+    for i in range(50):
+        subnode = Node([0,1], id="X2%s" % i)
+        node2_subnodes.append(subnode)
+
+    nodeX3 = Node([0,1], id="X3")
+    #Subtree of variables
+    node3_subnodes = []
+    for i in range(25):
+        subnode = Node([0,1], id="X3%s" % i)
+        node3_subnodes.append(subnode)
+
+    nodeX4 = Node([0,1], id="X4")
+    #Subtree of variables
+    node4_subnodes = []
+    for i in range(25):
+        subnode = Node([0,1], id="X4%s" % i)
+        node4_subnodes.append(subnode)
+    
+    #Parent child relationships
+    nodeY.add_parent(nodeX1)
+    nodeY.add_parent(nodeX2)
+    nodeY.add_parent(nodeX3)
+    nodeY.add_parent(nodeX4)
+
+    nodeX1.add_child(nodeY)
+    nodeX2.add_child(nodeY)
+    nodeX3.add_child(nodeY)
+    nodeX4.add_child(nodeY)
+
+    #Node 2 subtree
+    for n2_node in node2_subnodes:
+        nodeX2.add_parent(n2_node)
+        n2_node.add_child(nodeX2)
+    
+    #Node 3 subtree
+    for n3_node in node3_subnodes:
+        nodeX3.add_parent(n3_node)
+        n3_node.add_child(nodeX3)
+
+    #Node 4 subtree
+    for n4_node in node4_subnodes:
+        nodeX4.add_parent(n4_node)
+        n4_node.add_child(nodeX4)
+
+    #Conditional probability tables
+    nodeY.cpt = {(0,0,0,0):[0.99, 0.01], (0,0,0,1):[0.99, 0.01], (0,0,1,0):[0.99, 0.01], (0,0,1,1):[0.01, 0.99], (0,1,0,0):[0.99, 0.01], (0,1,0,1):[0.01, 0.99], (0,1,1,0):[0.01, 0.99], (0,1,1,1):[0.99, 0.01], (1,0,0,0):[0.99, 0.01], (1,0,0,1):[0.99, 0.01], (1,0,1,0):[0.99, 0.01], (1,0,1,1):[0.99, 0.01], (1,1,0,0):[0.01, 0.99], (1,1,0,1):[0.99, 0.01], (1,1,1,0):[0.99, 0.01], (1,1,1,1):[0,1]}
+
+    #Node 2 and subtree
+    #------------------
+    nodeX2.cpt = {}
+
+    var_tuples = nodeX2.get_variable_combinations()
+    for v_t in var_tuples:
+        rand_val = random.random()
+        nodeX2.cpt[v_t] = [rand_val, 1-rand_val]
+
+    for n2_node in node2_subnodes:
+        rand_val = random.random()
+        n2_node.cpt = {None:[rand_val, 1-rand_val]}
+    #------------------
+
+    #Node 3 and subtree
+    #------------------
+    nodeX3.cpt = {}
+
+    var_tuples = nodeX3.get_variable_combinations()
+    for v_t in var_tuples:
+        rand_val = random.random()
+        nodeX3.cpt[v_t] = [rand_val, 1-rand_val]
+
+    for n3_node in node3_subnodes:
+        rand_val = random.random()
+        n3_node.cpt = {None:[rand_val, 1-rand_val]}
+    #------------------
+    
+    #Node 4 and subtree
+    #------------------
+    nodeX4.cpt = {}
+
+    var_tuples = nodeX4.get_variable_combinations()
+    for v_t in var_tuples:
+        rand_val = random.random()
+        nodeX4.cpt[v_t] = [rand_val, 1-rand_val]
+
+    for n4_node in node4_subnodes:
+        rand_val = random.random()
+        n4_node.cpt = {None:[rand_val, 1-rand_val]}
+    #------------------
+
+    #Create network
+    #List of nodes in topological sorted order
+    nodes = []
+    nodes.extend(node4_subnodes)
+    nodes.extend(node3_subnodes)
+    nodes.extend(node2_subnodes)
+    nodes.extend(nodeX2)
+    nodes.extend(nodeX3)
+    nodes.extend(nodeX4)
+    nodes.extend(nodeX1)
+    nodes.extend(nodeY)
+
+    aBN = BeliefNetwork(nodes)
+    
+    #Sample network
+    samples = [aBN.sample() for x in range(10000)]
+
+    return samples
+    
+
+def notebook_data_1():
+
+    """ 
+        One Thousand variables.
+        One of the variables we're interested in producing a classifier for. 
+    """
+
+    #Create network
+    nodeY = Node([0,1], id="Y")
+    
+    nodeX1 = Node([0,1], id="X1")
+    nodeX2 = Node([0,1], id="X2")
+    nodeX3 = Node([0,1], id="X3")
+    nodeX4 = Node([0,1], id="X4")
+    nodeX5 = Node([0,1], id="X5")
+    nodeX6 = Node([0,1], id="X6")
+    nodeX7 = Node([0,1], id="X7")
+    nodeX8 = Node([0,1], id="X8")
+    nodeX9 = Node([0,1], id="X9")
+    nodeX10 = Node([0,1], id="X10")
+
+    #Parents
+    nodeY.add_parent(nodeX1)
+    nodeY.add_parent(nodeX2)
+    nodeY.add_parent(nodeX3)
+    nodeY.add_parent(nodeX4)
+
+    nodeX1.add_parent(nodeX7)
+    nodeX1.add_parent(nodeX8)
+
+    nodeX3.add_parent(nodeX5)
+
+    nodeX4.add_parent(nodeX6)
+    
+    nodeX6.add_parent(nodeX10)
+    nodeX6.add_parent(nodeX9)
+
+    #Children
+    nodeX1.add_child(nodeY)
+    nodeX2.add_child(nodeY)
+
+    nodeX7.add_child(nodeX1)
+    nodeX8.add_child(nodeX1)
+
+    nodeX3.add_child(nodeY)
+    nodeX4.add_child(nodeY)
+    nodeX5.add_child(nodeX3)
+    nodeX6.add_child(nodeX4)
+    nodeX10.add_child(nodeX6)
+    nodeX9.add_child(nodeX6)
+
+    #CPTs
+    
+    nodeY.cpt = {(0,0,0,0):[0.99, 0.01], (0,0,0,1):[0.99, 0.01], (0,0,1,0):[0.99, 0.01], (0,0,1,1):[0.99, 0.01], (0,1,0,0):[0.99, 0.01], (0,1,0,1):[0.99, 0.01], (0,1,1,0):[0.99, 0.01], (0,1,1,1):[0.99, 0.01], (1,0,0,0):[0.99, 0.01], (1,0,0,1):[0.99, 0.01], (1,0,1,0):[0.99, 0.01], (1,0,1,1):[0.99, 0.01], (1,1,0,0):[0.99, 0.01], (1,1,0,1):[0.99, 0.01], (1,1,1,0):[0.99, 0.01], (1,1,1,1):[0,1]}
+
+    nodeX1.cpt = {(0,0):[0.99, 0.01], (0,1):[0.99, 0.01], (1,0):[0.99, 0.01], (1,1):[0,1]}
+    
+    nodeX2.cpt = {None:[0.5, 0.5]}
+    nodeX3.cpt = {(0,):[0.99, 0.01], (1,):[0.2, 0.8]}
+    nodeX4.cpt = {(0,):[0.99, 0.01], (1,):[0.99, 0.01]}
+    nodeX5.cpt = {None:[0.5, 0.5]}
+    nodeX6.cpt = {(0,0):[0.99, 0.01], (0,1):[0, 1], (1,0):[0,1], (1,1):[0.99, 0.01]}
+    nodeX7.cpt = {None:[0.5, 0.5]}
+    nodeX8.cpt = {None:[0.5, 0.5]}
+    nodeX9.cpt = {None:[0.5, 0.5]}
+    nodeX10.cpt = {None:[0.5, 0.5]}
+
+    #Create and sample network
+    nodes = [nodeX7, nodeX8, nodeX5, nodeX10, nodeX9, nodeX6, nodeX1, nodeX2, nodeX3, nodeX4, nodeY]
+    aBN = BeliefNetwork(nodes)
+    samples = [aBN.sample() for x in range(10000)]
+
+    return samples
+
 
 def test_data():
 
